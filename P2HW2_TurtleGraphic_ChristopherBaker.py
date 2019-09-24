91edf6bb5b2927d5e3b9cbb667147b3f952a83c5
BakerC6085 <55755475+BakerC6085@users.noreply.github.com>
2019-09-24 11:46:56 -0400

    first submission
    second homework assignment of module 3

---
+++ P2HW2_TurtleGraphic_ChristopherBaker.py
@@ -0,0 +1,29 @@
+# Making a turtle graphic is Python
+# 9/24/19
+# CTI-110 P2HW2 - Turtle Graphic
+# Christopher Baker
+
+# Starting Turtle
+import turtle
+# coloring the shape
+turtle.fillcolor('blue')
+turtle.begin_fill()
+# Drawing the shape
+turtle.left(45)
+turtle.forward(150)
+turtle.right(90)
+turtle.forward(150)
+turtle.right(90)
+turtle.forward(150)
+turtle.right(90)
+turtle.forward(300)
+turtle.left(90)
+turtle.forward(150)
+turtle.left(90)
+turtle.forward(150)
+turtle.left(90)
+turtle.forward(150)
+turtle.end_fill()
+# keeping the turtle open when done witht he program.
+turtle.done()
+
