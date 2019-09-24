973d3c2eb7a257d14fc0d1b6764d5f8f71e762f5
BakerC6085 <55755475+BakerC6085@users.noreply.github.com>
2019-09-24 11:45:10 -0400

    First Submission
    tutorial program of module 2

---
+++ P1T1_BakerChristopher.py
@@ -0,0 +1,11 @@
+Python 3.7.4 (tags/v3.7.4:e09359112e, Jul  8 2019, 19:29:22) [MSC v.1916 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print('This is a test of IDLE.')
+This is a test of IDLE.
+>>> print('This is a test of IDLE.'
+
+
+
+      )
+This is a test of IDLE.
+>>> 
