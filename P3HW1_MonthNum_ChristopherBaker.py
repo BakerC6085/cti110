74ae8da4d38b9fb26c8b63bb44702f1584f91902
BakerC6085 <55755475+BakerC6085@users.noreply.github.com>
2019-10-03 17:44:01 -0400

    First Sumbission

---
+++ P3HW1_MonthNum_ChristopherBaker.py
@@ -0,0 +1,35 @@
+# CTI-110
+# P3HW1 - Month number
+# Christopher Baker
+# 10/3/2019
+#
+# Get the month number.
+MonthNum = int(input('Enter the month number 1 thur 12: '))
+
+# Determine the month from the number input.
+if MonthNum == 1:
+    print('The Month is January.')
+elif MonthNum == 2:
+    print('The Month if Feburary.')
+elif MonthNum == 3:
+    print('The Month is March.')
+elif MonthNum == 4:
+    print('The Month is April.')
+elif MonthNum == 5:
+    print('The Month is May.')
+elif MonthNum == 6:
+    print('The Month is June.')
+elif MonthNum == 7:
+    print('The Month is July.')
+elif MonthNum == 8:
+    print('The Month is August.')
+elif MonthNum == 9:
+    print('The Month is September.')
+elif MonthNum == 10:
+    print('The Month is October.')
+elif MonthNum == 11:
+    print('The Month is November.')
+elif MonthNum == 12:
+    print('The Month is December.')
+else:
+    print('Error please enter number from 1 to 12')
