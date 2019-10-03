55daa0f501e2f733f8148d8c6138b7288647b935
BakerC6085 <55755475+BakerC6085@users.noreply.github.com>
2019-10-03 18:20:23 -0400

    first submission

---
+++ P3LAB_Debugging_ChristopherBaker.py
@@ -0,0 +1,23 @@
+# CTI-110
+# P3LAB - Debugging
+# Christopher Baker
+# 10/3/2019
+#
+# Setting up if elif else statements with proper indentation.
+if score >= A_score:
+    print('Your grade is A.')
+else:
+    if score >= B_score:
+        print('Your grade is B.')
+     else:
+        if score >= C_score:
+            print('Your grade is C.)
+        else:
+            if score >= D_score:
+                print('Your score is D.')
+            else:
+                if score >= F_score:
+                    print('Your score is F.')
+                                
+                                      
+                              
